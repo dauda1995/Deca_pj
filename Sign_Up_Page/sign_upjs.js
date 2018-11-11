11aeab3516a0dcf237d4162b615ee6082561a108
dauda1995 <dauda933@gmail.com>
2018-11-11 16:18:24 +0100

    newly added

---
+++ Sign_Up_Page/sign_upjs.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    let arr = []
+    function usersInfo(email, username, password){
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    function find_email(str){
+        let status = true;
+        for( let i = 0; i<arr.length;i++){
+            if(arr[i] == str){
+               // alert('this user already exists')
+                status = true;
+                break;
+                
+        }else{
+           // alert('still checking')
+            status = false;
+        }
+        }
+        return status;
+    }
+   
+    $('.signbtn').click(function(){
+        alert( 'this is working')
+
+        //get user info from html elements
+        let emailId = $('#email').val()
+        let userId = $('#username').val()
+        let passwordId = $('#pass').val()
+                
+        
+        //create user object
+        var user =new usersInfo(emailId, userId, passwordId);
+        let emailstr= user.email
+               
+        //get request to get users info from database
+        $.get(' http://localhost:3000/users', function(data){
+            alert('okay, this too')
+            $.each(data, function(i,single){
+               // alert(single.email)
+                arr.push(single.email)
+            
+            })
+            alert(arr)
+           
+            if(find_email(emailstr)==true){
+                alert('this user already exists')
+                arr = []
+            }else{
+                alert('no users')
+                arr=[]
+                $.post("http://localhost:3000/users",user)
+                export {emailstr}
+            }   
+
+            
+                
+                
+            
+        })
+    
+    })
+})
